adc7fc783706a0aabbf6be25a58aa545cea5d99f
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2023-12-05 19:28:24 +0000

    Update script.js

--- Day 2/script.js
+++ Day 2/script.js
@@ -1,62 +1,106 @@
-// import { rawData } from "./rawData.js";
+import { rawData } from "./rawData.js";
 
-const rawData = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+// const rawData = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+// Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+// Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+// Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
 
 const rowData = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green`;
 
-const cubes = {
-  red: 12,
-  green: 13,
-  blue: 14,
-};
-
-const turnData = [" 6 red, 1 blue, 3 green", " 2 blue, 1 red, 2 green"];
+const turnData = ["6 red, 1 blue, 3 green"];
 
 const processed = rawData.split(/\n/);
 
-const rowDataSplit = rowData.split(/:|;/);
-
 const processTurn = function (game) {
-  const procGame = game.split(/,/);
-  console.log(procGame);
+  const procGame = game.split(/, /);
+  // console.log(procGame);
+  const cubes = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+  let possible = 0;
+  procGame.forEach((colour) => {
+    const numberColour = colour.split(/ [a-zA-Z]/);
+    if (colour.includes("red")) {
+      numberColour[0] > cubes.red ? (possible += 1) : (possible += 0);
+    } else if (colour.includes("green")) {
+      numberColour[0] > cubes.green ? (possible += 1) : (possible += 0);
+    } else if (colour.includes("blue")) {
+      numberColour[0] > cubes.blue ? (possible += 1) : (possible += 0);
+    }
+  });
+
+  return possible;
 };
 
-const processedTurns = turnData.map(processTurn);
+// const processedTurns = turnData.map(processTurn);
 
 const processGame = function (row) {
-  const processRow = row.split(/:|;/);
+  const processRow = row.split(/: |; /);
   const game = processRow.shift();
-  const gameNumber = game.slice(-1);
-  const turn = processRow.map();
-  // console.log(processRow[1]);
-  // const colours = processRow.split(/,/);
-  // console.log(colours);
+
+  const gameNumber = game.match(/\d+/g)[0];
+  const turn = processRow
+    .map((turn) => processTurn(turn))
+    .reduce((accumulator, currentValue) => accumulator + currentValue);
+  if (turn == 0) {
+    return parseInt(gameNumber);
+  } else {
+    return 0;
+  }
 };
 
-// const mapped = processed.map(processGame);
+const processAll = processed
+  .map((row) => processGame(row))
+  .reduce((accumulator, currentValue) => accumulator + currentValue);
 
-// console.log(processed);
+// Part 1 Answer
+// console.log(processAll);
 
-const dataAsObject = {
-  Cubes: {
-    Game: 1,
-    Turn: {
-      Result: {
-        number: 1,
-        blue: 0,
-        green: 0,
-        red: 0,
-      },
-      Result: {
-        number: 1,
-        blue: 0,
-        green: 0,
-        red: 0,
-      },
-    },
-  },
+const countTurn = function (game) {
+  const procGame = game.split(/, /);
+  return procGame;
 };
+
+const countGame = function (row) {
+  const processRow = row.split(/: |; /);
+
+  const game = processRow.shift();
+
+  const gameNumber = game.match(/\d+/g)[0];
+
+  const turn = processRow.map((turn) => countTurn(turn)).flat();
+
+  console.log(turn);
+
+  const cubes = {
+    red: 0,
+    green: 0,
+    blue: 0,
+  };
+
+  turn.forEach((colour) => {
+    const numberColour = colour.split(/ [a-zA-Z]/);
+    const number = parseInt(numberColour[0]);
+    if (colour.includes("red")) {
+      number > cubes.red ? (cubes.red = number) : (cubes.red += 0);
+    } else if (colour.includes("green")) {
+      number > cubes.green ? (cubes.green = number) : (cubes.green += 0);
+    } else if (colour.includes("blue")) {
+      number > cubes.blue ? (cubes.blue = number) : (cubes.blue += 0);
+    }
+  });
+
+  // use reduce to multiply the contents of the array
+  const answer = cubes.red * cubes.blue * cubes.green;
+
+  return answer;
+};
+
+const countAll = processed
+  .map((row) => countGame(row))
+  .reduce((accumulator, currentValue) => accumulator + currentValue);
+
+console.log(countAll);
