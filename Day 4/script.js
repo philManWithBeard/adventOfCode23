3f32b33dd415a3a1547149abce6c5aa2bbfd8c36
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2023-12-11 22:40:43 +0000

    part 2 array

--- Day 4/script.js
+++ Day 4/script.js
@@ -1,4 +1,11 @@
-import { rawData } from "./rawData.js";
+// import { rawData } from "./rawData.js";
+
+const rawData = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
 
 const cardObjects = (card) => {
   const cardNumber = card.match(/\d+(?=:)/)[0];
@@ -25,14 +32,17 @@ const calculatePoints = (matchingNumber) => {
   return points;
 };
 
-const winCount = (wins, i) => {
+const cardWin = (wins, i) => {
   const winCount = wins.length;
   const card = i + 1;
   const cardCount = 1;
   return { card, cardCount, winCount };
 };
 
-const cardAdd = () => {};
+const cardAdd = (cardWinCount, i) => {
+  const winCount = cardWinCount.winCount;
+  for (let x = 0; x < winCount; x++) {}
+};
 
 const part1 = rawData
   .split(/\n/)
@@ -42,6 +52,10 @@ const part1 = rawData
   .filter((a) => a >= 1)
   .reduce((acc, cur) => acc + cur);
 
-const part2 = rawData.split(/\n/).map(cardObjects).map(punterWin).map(winCount);
+const part2array = rawData
+  .split(/\n/)
+  .map(cardObjects)
+  .map(punterWin)
+  .map(cardWin);
 
 console.log(part2);
